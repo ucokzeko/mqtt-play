b045adfc0b560352ec0f0a45b6c544d59caa740e
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-04-18 14:57:52 +1000

    QUALITY: Use const for client variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mqtt = require('mqtt');
 const config = require('config.json')('./config.json');
 const player = require('./module/player.js')();
 
-let client = mqtt.connect('mqtt://localhost');
+const client = mqtt.connect('mqtt://localhost');
 
 client.on('connect', function () {
   client.subscribe(config.channel.sub);
