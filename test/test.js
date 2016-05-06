ec6d68f92427e5e215b2e9a0ad3b79dafdac0262
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-05-06 13:18:51 +1000

    NEW: Add test script and audio file

---
+++ test/test.js
@@ -0,0 +1,48 @@
+const mqtt    = require('mqtt');
+const winston = require('winston');
+const spawn   = require('child_process').spawn;
+const client  = mqtt.connect('mqtt://localhost');
+const config  = require('config.json')('./src/config.json');
+
+const mqttConfig = { qos: 1 };
+
+launchService()
+.then(() => {
+  publishToPlayer();
+});
+
+function launchService() {
+  return new Promise((fullfil) => {
+    winston.info('Launching player service...');
+    const service = spawn('npm', ['start']);
+
+    service.stdout.on('data', (data) => {
+      winston.info(`MQTT-Play log: ${data}`);
+      if (data.indexOf(config.topic.sub) > -1) {
+        fullfil();
+      }
+      if (data.indexOf('Finished playing') > -1) {
+        winston.info('Test succeeded');
+        process.exit();
+      }
+    });
+
+    service.stderr.on('data', (data) => {
+      winston.info(`MQTT-Play log: ${data}`);
+    });
+
+    service.on('close', (code) => {
+      winston.info(`MQTT-Play exited with code ${code}`);
+    });
+  });
+}
+
+function publishToPlayer() {
+  winston.info(`Publishing message to topic ${config.topic.sub}`);
+  const message = `${__dirname}/audio/test.mp3`;
+  client.publish(config.topic.sub, message, mqttConfig);
+  setTimeout(() => {
+    winston.error(new Error('Test failed (Waiting timeout)'));
+    process.exit();
+  }, 10000);
+}
