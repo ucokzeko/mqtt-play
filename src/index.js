520b988e6c4289eece0e544e4ebc3764b4e208f2
Rhys <rimmington@gmail.com>
2016-05-11 14:38:55 +1000

    ENHANCE: Get play command from environment variable
    This will make it easier to run on different platforms as a service.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const winston = require('winston');
 
 const client = mqtt.connect('mqtt://localhost');
 
-client.on('connect', () => {
-  client.subscribe(config.topic.sub);
-  winston.info(`Subscribed topic: ${config.topic.sub}`);
-});
+if (process.env.PLAY_COMMAND) {
+  winston.info(`Player command: ${process.env.PLAY_COMMAND}`);
+  client.on('connect', () => {
+    client.subscribe(config.topic.sub);
+    winston.info(`Subscribed topic: ${config.topic.sub}`);
+  });
 
-client.on('message', (topic, message) => {
-  winston.info(`Message received: ${message.toString()}`);
-  player.addAudio(message.toString());
-});
+  client.on('message', (topic, message) => {
+    winston.info(`Message received: ${message.toString()}`);
+    player.addAudio(message.toString());
+  });
+} else {
+  winston.error('Missing PLAY_COMMAND environment variable');
+}
