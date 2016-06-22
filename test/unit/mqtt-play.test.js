69a043706a19c2fb6bba1d73a6fe6faea39fc2dd
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-06-22 13:40:26 +1000

    ENHANCE: Check if PLAY_COMMAND is available before running unit test

--- test/unit/mqtt-play.test.js
+++ test/unit/mqtt-play.test.js
@@ -1,31 +1,35 @@
 /* eslint-disable */
 const assert = require('assert');
 
-describe('mqtt-play', () => {
-  describe('Player#addAudio()', () => {
-    const player = require('${__dirname}/../../src/module/player.js')();
-    it('should play audio without an error when file exists', (done) => {
-      assert.doesNotThrow(() => {
-        player.addAudio(`${__dirname}/../audio/test.mp3`);
-      },
-      (error) => {
-        if (error instanceof Error) {
-          return error;
-        } else {
-          return false;
+if (process.env.PLAY_COMMAND) {
+  describe('mqtt-play', () => {
+    describe('Player#addAudio()', () => {
+      const player = require('${__dirname}/../../src/module/player.js')();
+      it('should play audio without an error when file exists', (done) => {
+        assert.doesNotThrow(() => {
+          player.addAudio(`${__dirname}/../audio/test.mp3`);
+        },
+        (error) => {
+          if (error instanceof Error) {
+            return error;
+          } else {
+            return false;
+          }
         }
-      }
-      );
-      done();
-    });
+        );
+        done();
+      });
 
-    it('should throw \'no such file\' error when when file doesn\'t exist', (done) => {
-      assert.throws(() => {
-        player.addAudio(`${__dirname}/../audio/audio.mp3`);
-      },
-      /no such file or directory/,
-      'Function does throw');
-      done();
+      it('should throw \'no such file\' error when when file doesn\'t exist', (done) => {
+        assert.throws(() => {
+          player.addAudio(`${__dirname}/../audio/audio.mp3`);
+        },
+        /no such file or directory/,
+        'Function does throw');
+        done();
+      });
     });
   });
-});
+} else {
+  throw new Error('Missing PLAY_COMMAND environment variable');
+}
