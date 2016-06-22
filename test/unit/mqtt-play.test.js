5dda3fb4aaf3b72844eef34f55a4262e3bc76807
Stefanus Kurniawan <zt.ucokzeko@gmail.com>
2016-06-22 14:11:40 +1000

    QUALITY: Use guard-style to check env variable

--- test/unit/mqtt-play.test.js
+++ test/unit/mqtt-play.test.js
@@ -1,35 +1,30 @@
 /* eslint-disable */
 const assert = require('assert');
 
-if (process.env.PLAY_COMMAND) {
-  describe('mqtt-play', () => {
-    describe('Player#addAudio()', () => {
-      const player = require('${__dirname}/../../src/module/player.js')();
-      it('should play audio without an error when file exists', (done) => {
-        assert.doesNotThrow(() => {
-          player.addAudio(`${__dirname}/../audio/test.mp3`);
-        },
-        (error) => {
-          if (error instanceof Error) {
-            return error;
-          } else {
-            return false;
-          }
-        }
-        );
-        done();
-      });
+if (!process.env.PLAY_COMMAND) { throw new Error('PLAY_COMMAND environment variable not set'); }
 
-      it('should throw \'no such file\' error when when file doesn\'t exist', (done) => {
-        assert.throws(() => {
-          player.addAudio(`${__dirname}/../audio/audio.mp3`);
-        },
-        /no such file or directory/,
-        'Function does throw');
-        done();
-      });
+describe('mqtt-play', () => {
+  describe('Player#addAudio()', () => {
+    const player = require('${__dirname}/../../src/module/player.js')();
+    it('should play audio without an error when file exists', (done) => {
+      assert.doesNotThrow(() => {
+        player.addAudio(`${__dirname}/../audio/test.mp3`);
+      },
+      (error) => {
+        if (error instanceof Error) { return error; }
+        return false;
+      }
+      );
+      done();
+    });
+
+    it('should throw \'no such file\' error when when file doesn\'t exist', (done) => {
+      assert.throws(() => {
+        player.addAudio(`${__dirname}/../audio/audio.mp3`);
+      },
+      /no such file or directory/,
+      'Function does throw');
+      done();
     });
   });
-} else {
-  throw new Error('Missing PLAY_COMMAND environment variable');
-}
+});
